43ac447dd67fb4486f333456841c3fe8e60784c1
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2024-12-03 23:46:20 +0100

    Add tailwind, color-palette, basic strucutre for the app, etc.
    Co-authored-by: Hadock <Hadock-is-ok@users.noreply.github.com>

---
+++ src/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div class="flex h-screen w-screen items-center justify-center">
+            <div class="text-6xl font-bold">Hello, world!</div>
+            <a href="/test">192.168.1.1 doxxed</a>
+        </div>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: "selector",
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                "light-bg": "#e4e4e7" /* Light Mode */,
+                "dark-bg": "#0a0a0a" /* Dark Mode */,
+                "night-bg": "#000000" /* AMOLED (or Midnight) Mode*/,
+                "light-bg-text": "#171717",
+                "dark-bg-text": "#a1a1aa",
+                "night-bg-text": "#d4d4d4",
+            },
+        },
+    },
+    plugins: [],
+};
