83d3edf29a2dc15f23b8e76576a96a0ca828ecda
cyrus01337 <cyrus01337@gmail.com>
2024-12-08 04:01:00 +0000

    Prevent empty messages from being sent

--- src/components/ChatBar.tsx
+++ src/components/ChatBar.tsx
@@ -1,21 +1,35 @@
 import type { JSX, Setter } from "solid-js";
 
 export default function ChatBar({ setMessages }: { setMessages: Setter<Message[]> }) {
+  let elementReference!: HTMLInputElement;
+
   const addMessage: JSX.EventHandler<HTMLInputElement, KeyboardEvent> = event => {
+    const contentFound = event.currentTarget.value;
+
     if (event.key !== "Enter") {
+      return;
+    } else if (!contentFound.trim()) {
+      elementReference.value = "";
+
       return;
     }
 
     const newMessage: Message = {
       id: 1,
       author: "Big Balls Jr. Sr.",
-      content: event.currentTarget.value,
+      content: contentFound,
     };
 
     setMessages(previousMessages => [...previousMessages, newMessage]);
+    elementReference.value = "";
   };
 
   return (
-    <input class="rounded-lg p-3 text-black outline-none" type="text" onKeyPress={addMessage} />
+    <input
+      class="rounded-lg p-3 text-black outline-none"
+      ref={elementReference}
+      type="text"
+      onKeyPress={addMessage}
+    />
   );
 }
