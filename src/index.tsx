7a8babd4a49f78418e77b582016906cf62e705be
Willi <83978878+itswilliboy@users.noreply.github.com>
2024-12-25 22:45:46 +0100

    New websockets (#6)
    * Remove standard function
    
    * Re-implement websocket on the frontend
    
    * Resolve issues
    
    * Fix rename of MessageT
    
    * Implement stores
    
    * Small fixes
    
    * Break newline
    
    * Resolve issues

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,16 @@ import "~/index.css";
 import "@fontsource/noto-sans";
 
 import RootLayout from "~/layouts/Root";
+import GlobalProvider from "~/libs/GlobalProvider";
 import Home from "~/pages/Home";
 
 render(
   () => (
-    <Router base="/app" root={RootLayout}>
-      <Route path="/" component={Home} />
-    </Router>
+    <GlobalProvider>
+      <Router base="/app" root={RootLayout}>
+        <Route path="/" component={Home} />
+      </Router>
+    </GlobalProvider>
   ),
   document.getElementById("root") as HTMLElement,
 );
