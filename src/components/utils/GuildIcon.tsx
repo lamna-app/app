dfff69fb60cace6b366718e866f37332cf042a4c
itswilliboy <83978878+itswilliboy@users.noreply.github.com>
2026-04-02 14:10:24 +0200

    fix: fix typo

--- src/components/utils/GuildIcon.tsx
+++ src/components/utils/GuildIcon.tsx
@@ -1,7 +1,7 @@
 export default function GuildIcon({ name }: { name: string }) {
   const letters = name.split(" ").map(word => word[0].toUpperCase())
   return (
-    <div class="bg-light text-22l grid size-13.5 place-items-center font-semibold hover:rounded-2xl">
+    <div class="bg-light text-lg grid size-13.5 place-items-center font-semibold hover:rounded-2xl">
       {letters.slice(0, 4)}
     </div>
   )
