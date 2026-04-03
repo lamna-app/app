182bf7a46bbf10b1dd0cc2065886c3aa506840ce
itswilliboy <83978878+itswilliboy@users.noreply.github.com>
2026-04-03 21:51:04 +0200

    feat: Implement more socket logic
    Co-authored-by: Faaz M <du-cki@users.noreply.github.com>

--- src/libs/socket.ts
+++ src/libs/socket.ts
@@ -7,11 +7,30 @@ export class Socket {
   private ws?: WebSocket
   private _handlers: Map<string, EventHandler<unknown>[]> = new Map()
 
+  private _heartbeatInterval: Option<number> = null
+  private _heartbeat = 30 // seconds
+
   connect(token: string) {
     this.ws = new WebSocket(`${Socket.BASE}/v${Socket.VERSION}/ws?token=${token}`)
 
-    this.ws.onopen = () => console.log("Websocket connected")
-    this.ws.onclose = () => console.log("Websocket disconnected")
+    this.ws.onopen = () => {
+      console.log("Websocket connected")
+
+      this._heartbeatInterval = setInterval(() => {
+        if (this.isOpen()) {
+          this.ws?.send(JSON.stringify({ op: 3, e: "ping" }))
+        }
+      }, this._heartbeat * 1000)
+    }
+
+    this.ws.onclose = () => {
+      console.log("Websocket disconnected")
+
+      if (this._heartbeatInterval) {
+        clearInterval(this._heartbeatInterval)
+      }
+    }
+
     this.ws.onerror = err => console.error("Websocket error:", err)
 
     this.ws.onmessage = event => {
