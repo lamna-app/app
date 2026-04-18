d14ec472b1d66c8267f901a65209d893a7138d5b
itswilliboy <83978878+itswilliboy@users.noreply.github.com>
2026-04-18 22:25:16 +0200

    refactor: modify common input

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -2,20 +2,24 @@ import type { JSX } from "solid-js"
 
 type Props = { label: string } & JSX.InputHTMLAttributes<HTMLInputElement>
 
-export default function InputField({ label, name, ...props }: Props) {
+export default function InputField({ label, name, value, children, ...props }: Props) {
   return (
     <div class="flex flex-col gap-1">
       <label for={name} class="text-sm font-medium select-none">
         {label}
       </label>
 
-      <input
-        id={name}
-        name={name}
-        autocomplete="off"
-        {...props}
-        class="bg-dark-hl rounded-lg p-2 outline-none transition-all focus:ring-2 focus:ring-light disabled:opacity-50 disabled:pointer-events-auto disabled:bg-red-300"
-      />
+      <div class="relative">
+        <input
+          id={name}
+          name={name}
+          value={value ?? ""}
+          autocomplete="off"
+          {...props}
+          class="bg-dark-hl rounded-lg w-full p-2 outline-none transition-all focus:ring-2 focus:ring-light disabled:opacity-50 disabled:pointer-events-auto disabled:bg-red-300"
+        />
+        {children}
+      </div>
     </div>
   )
 }
