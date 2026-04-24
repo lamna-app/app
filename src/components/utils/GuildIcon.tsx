2afc46a5a0bc8ac029e355ad77f209b1f7403abf
du-cki <70286349+du-cki@users.noreply.github.com>
2026-04-24 20:45:09 +0400

    fix: trailing space error

--- src/components/utils/GuildIcon.tsx
+++ src/components/utils/GuildIcon.tsx
@@ -1,5 +1,8 @@
 export default function GuildIcon(props: { name: string }) {
-  const letters = props.name.split(" ").map(word => word[0].toUpperCase())
+  const letters = props.name
+    .trim()
+    .split(" ")
+    .map(word => word[0].toUpperCase())
 
   return <span class="text-lg font-semibold">{letters.slice(0, 4).join("")}</span>
 }
